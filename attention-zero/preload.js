b93a9e89d98c3b55f583c8e77cff0819f1077826
hemi <hnngggrrrr@gmail.com>
2023-11-11 10:42:00 +0800

    subway surfers + radio button

--- attention-zero/preload.js
+++ attention-zero/preload.js
@@ -5,9 +5,7 @@ const { exec, spawn } = require("child_process");
 let popUp = false
 
 window.addEventListener('DOMContentLoaded', () => {
-  ipcRenderer.on('asynchronous-reply', (event, arg) => {
-    console.log(arg) // prints "pong"
-  })
+
   //button and its event listener
   const b1 = document.getElementById('b1');
   const b2 = document.getElementById('b2');
@@ -19,29 +17,30 @@ window.addEventListener('DOMContentLoaded', () => {
       const out = document.getElementById("out")
       console.log(filePath[0])
 
-      const ls = spawn("py", ['../python stuff/pixel.py', '--vidPath', '../attention-zero/video/videos/Renai Circulation.mp4', '--percentile', '70', '--vid2height', '480', '--vid2width', '360', '--attentionSpan', '300'])
+      const python = spawn("py", ['../python stuff/pixel.py', '--vidPath', '../attention-zero/video/videos/Renai Circulation.mp4', '--percentile', '70', '--vid2height', '480', '--vid2width', '360', '--attentionSpan', '300'])
 
       out.innerText += "it is the working"
 
-      ls.stdout.on('data', (data) => {
+      python.stdout.on('data', (data) => {
         out.innerText += `stdout: ${data}`
       });
 
-      ls.stderr.on('data', (data) => {
+      python.stderr.on('data', (data) => {
         out.innerText += `stderr: ${data}`
       });
 
-      ls.on('close', (code) => {
+      python.on('close', (code) => {
         out.innerText += `child process exited with code ${code}`
       });
     })
   })
   b2.addEventListener('click', () => {
     popUp = !popUp;
+    myCallback()
+    const intervalID = setInterval(myCallback, 100 * 1000);
   })
 })
 
-const intervalID = setInterval(myCallback, 5 * 1000);
 function myCallback() {
   if (popUp) ipcRenderer.send('asynchronous-message')
 }
